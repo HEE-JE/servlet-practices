a617fd61374660b64a715d285fb8c1495f95ab52
HEE-JE <windba78@naver.com>
2022-05-23 16:43:30 +0900

    sql

---
+++ sql/guestbook.sql
@@ -0,0 +1,12 @@
+-- guestbook
+
+desc guestbook;
+
+-- insert
+insert into guestbook values(null, '희제', '1234', '안녕하세요', date_format(now(), '%Y-%m-%d %r'));
+
+-- delete
+delete from guestbook where no = 1 and password = 1234;
+
+-- select
+select no, name, date_format(reg_date, '%Y-%m-%d %r'), message from guestbook order by no desc;
